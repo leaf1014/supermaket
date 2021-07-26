8508f5f826bc3a2fe23e87bd49f7e47afea7dac6
unknown <979177653@qq.com>
2021-07-26 18:41:04 +0800

    leaf

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div id="nav">
-    <h2>Hello</h2>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
-  <router-view/>
 </template>
 
+<script>
+import mainTabBar from "components/context/maintabbar/mainTabBar";
+export default {
+  name: 'App',
+  components: {
+    mainTabBar
+  }
+}
+</script>
+
 <style>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Home =  () => import('views/home/Home')
+const Category =  () => import('views/category/Category')
+const Cart =  () => import('views/cart/Cart')
+const Profile =  () => import('views/profile/Profile')
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:'',
+    redirect:'/home'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component:Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component:Cart
+  },
+  {
+    path: '/profile',
+    component:Profile
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
